89205d3ed0c8db2e7f5b856954b9a0748e5f4f42
Ayush Garg <ayush181000@gmail.com>
2022-09-03 05:18:18 +0530

    normal graphql call

--- client/src/components/ChatScreen.js
+++ client/src/components/ChatScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import {
   AppBar,
@@ -12,6 +12,42 @@ import MessageCard from './MessageCard';
 
 const ChatScreen = () => {
   const { id, name } = useParams();
+  const [messages, setMessages] = useState([]);
+
+  const getAllMessages = () => {
+    fetch('http://localhost:4000/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY2MjA3OTA1MH0.BYlgfZMf36BJvtXkeKQ6_9AEDE1Nog4bFmcr_Ps21ok',
+      },
+      body: JSON.stringify({
+        query: `
+        query MessagesByUser($messagesByUserReceiverId2: Int!) {
+          messagesByUser(receiverId: $messagesByUserReceiverId2) {
+            text
+            id
+            receiverId
+            senderId
+            createdAt
+          }
+        }
+        `,
+        variables: { messagesByUserReceiverId2: 3 },
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.data.messagesByUser);
+        // update state
+      });
+  };
+
+  useEffect(() => {
+    getAllMessages();
+  }, []);
+
   return (
     <Box flexGrow='1'>
       <AppBar position='static' sx={{ backgroundColor: 'white', boxShadow: 0 }}>
@@ -29,7 +65,7 @@ const ChatScreen = () => {
         backgroundColor='#f5f5f5'
         height='80vh'
         padding='10px'
-        overflowY='auto'
+        sx={{ overflowY: 'auto' }}
       >
         <MessageCard text='hi mukesh' date='1223' direction={'start'} />
         <MessageCard text='hi mukesh' date='1223' direction={'end'} />
